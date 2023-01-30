29127315b28a373db26642af5bd723d38d967e57
DevRenato <u18215194@gmail.com>
2023-01-30 18:51:00 -0500

    Animacion Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,34 +5,34 @@ const Header = () => {
     <header className="min-h-fit bg-white border-b-2">
         <div className="w-full container flex justify-between items-center px-10 py-5">
             <a
-            href="#"
-            className="text-gray-500 hover:text-gray-700 font-bold text-3xl transition-colors duration-500 cursor-pointer"
+              href="#"
+              className="text-gray-500 hover:text-gray-700 font-bold text-3xl transition-colors duration-500 cursor-pointer"
             >
-              {'<'}RenatoSoca{'/>'}
+              {'<'}RenatoSoca {'/>'}
             </a>
         
             <nav className="flex gap-5">
-                <a 
+                <a
                 href="#" 
-                className="text-gray-500 hover:text-gray-700 transition-colors duration-500 font-medium"
+                className="relative text-gray-500 hover:text-gray-700 transition-colors duration-500 font-medium after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-slate-500 after:transition-all hover:after:w-full"
                 >
                   Sobre Mí
                 </a>
                 <a 
                 href="#" 
-                className="text-gray-500 hover:text-gray-700 transition-colors duration-500 font-medium"
+                className="relative text-gray-500 hover:text-gray-700 transition-colors duration-500 font-medium after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-slate-500 after:transition-all hover:after:w-full"
                 >
                   Proyectos
                 </a>
                 <a 
                 href="#" 
-                className="text-gray-500 hover:text-gray-700 transition-colors duration-500 font-medium"
+                className="relative text-gray-500 hover:text-gray-700 transition-colors duration-500 font-medium after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-slate-500 after:transition-all hover:after:w-full"
                 >
                   Tecnologías
                 </a>
                 <a 
                 href="#" 
-                className="text-gray-500 hover:text-gray-700 transition-colors duration-500 font-medium"
+                className="relative text-gray-500 hover:text-gray-700 transition-colors duration-500 font-medium after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-slate-500 after:transition-all hover:after:w-full"
                 >
                   Contacto
                 </a>
