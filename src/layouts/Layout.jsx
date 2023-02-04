1ff150686bcec4a1b6c828a11251b1cbad698d96
DevRenato <u18215194@gmail.com>
2023-02-04 00:07:41 -0500

    Construyendo el Home

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,48 +1,48 @@
 import { Outlet, Link } from 'react-router-dom';
 import CV from '../assets/renato.pdf'
-import { AiFillMessage } from 'react-icons/ai';
+import { BiMailSend } from 'react-icons/bi';
 import { MdFileDownload } from "react-icons/md";
 
 const Layout = () => {
   return (
     <>
-      <header className="bg-white border-b-2">
-        <div className="w-full container flex justify-between items-center px-10 py-3 mx-auto">
+      <header className="bg-white fixed w-full z-10">
+        <div className=" w-full container flex justify-between items-center px-10 py-3 mx-auto">
           <Link
             to={'/'}
-            className="text-gray-500 hover:text-gray-700 transition-colors duration-[400ms] font-bold text-3xl"
+            className="text-black font-bold text-3xl"
           >
             <span className='italic'>{'<'}RenatoSoca {'/>'}</span>
           </Link>
       
-          <nav className="flex gap-3 text-gray-500 font-medium ">
+          <nav className="flex gap-3 text-black font-medium ">
             <Link
               to="/" 
-              className="relative hover:text-gray-700 transition-all duration-[400ms] after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-gray-700 after:duration-[400ms] hover:after:w-full"
+              className="relative transition-[width] after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-black after:duration-[400ms] hover:after:w-full"
             >
               Inicio
             </Link>
             <Link
               to="/informacion" 
-              className="relative hover:text-gray-700 transition-colors duration-[400ms] after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-gray-700 after:duration-[400ms] hover:after:w-full"
+              className="relative transition-[width] after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-black after:duration-[400ms] hover:after:w-full"
             >
               Sobre Mí
             </Link>
             <Link 
               to="/proyecto" 
-              className="relative hover:text-gray-700 transition-colors duration-[400ms] after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-gray-700 after:duration-[400ms] hover:after:w-full"
+              className="relative transition-[width] after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-black after:duration-[400ms] hover:after:w-full"
             >
               Proyectos
             </Link>
             <Link 
               to="/habilidades" 
-              className="relative hover:text-gray-700 transition-colors duration-[400ms] after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-gray-700 after:duration-[400ms] hover:after:w-full"
+              className="relative transition-[width] after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-black after:duration-[400ms] hover:after:w-full"
             >
               Habilidades
             </Link>
             <Link 
               to="/contacto" 
-              className="relative hover:text-gray-700 transition-colors duration-[400ms] after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-gray-700 after:duration-[400ms] hover:after:w-full"
+              className="relative transition-[width] after:absolute after:content-[''] after:w-0 after:left-0 after:h-0.5 after:-bottom-1 after:bg-black after:duration-[400ms] hover:after:w-full"
             >
               Contacto
             </Link>
@@ -51,19 +51,19 @@ const Layout = () => {
           <div className="flex gap-3 text-white font-medium">
             <Link
               to="/contacto"
-              className="flex items-center gap-2 border border-transparent bg-[#4DB5FF] text-white px-4 py-2 rounded  hover:bg-transparent hover:border-[#4DB5FF] hover:text-[#4DB5FF] transition-colors duration-[400ms]"  
+              className="flex items-center gap-2 border border-transparent bg-black text-white px-4 py-2 rounded hover:bg-transparent hover:border-black hover:text-black transition-colors duration-[400ms]"  
             >
               <span>Contáctame</span>
-              <AiFillMessage className='text-[1.35rem]'/>
+              <BiMailSend className='text-2xl'/>
             </Link>
 
             <a
               href={CV}
               download
-              className="flex items-center gap-2 border border-[#4DB5FF] text-[#4DB5FF] px-4 py-2 rounded hover:bg-[#4DB5FF] hover:text-white transition-colors duration-[400ms]"
+              className="flex items-center gap-2 border border-[#B887FD] text-[#B887FD] px-4 py-2 rounded hover:bg-[#B887FD] hover:text-white transition-colors duration-[400ms]"
             >
               Descargar CV
-              <MdFileDownload className='text-[1.5rem]'/>
+              <MdFileDownload className='text-2xl'/>
             </a>
           </div>
         </div>
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,35 @@
+import renato from '../assets/renato.png';
+
 const Index = () => {
   return (
     <>
-      <section className="container mx-auto px-10 pb-5">
-        <h2>Pagina Principal</h2>
+      <section className="relative container mx-auto px-10 pt-24 min-h-screen overflow-hidden flex gap-8 ">
+        <svg className="absolute z-10 -top-[3.5rem] left-[22.5%]" width="120" height="120">
+          <circle cx="60" cy="60" r="60" fill="#B887FD"/>
+        </svg>
+
+        <div className="flex-1 flex flex-col justify-center">
+          <h1>Info Personal</h1>
+        </div>
+
+        <div className="flex-1 flex flex-col justify-center items-center relative">
+          <svg className="blur" width="400" height="400">
+            <circle cx="200" cy="200" r="200" fill="#B887FD"/>
+          </svg>
+          <img className='absolute bottom-[-1rem] w-[50rem] object-cover' src={renato} alt="" />
+        </div>
+
+        <div className="flex-1 flex flex-col justify-center">
+          <h2>Sobre mi</h2>
+        </div>
+
+        <svg className="absolute -bottom-[3.5rem] left-[72.5%]" width="120" height="120">
+          <circle cx="60" cy="60" r="60" fill="#B887FD"/>
+        </svg>
+      </section>
+
+      <section className="min-h-screen">
+
       </section>
     </>
   );
